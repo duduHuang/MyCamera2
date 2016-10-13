f515c220d25a0ba38ef6086a358bea0985f1df85
NedHuang <NedHuang@via.com.tw>
2016-10-13 18:31:26 +0800

    Add camera open and preview function.
    Signed-off-by: NedHuang <NedHuang@via.com.tw>

--- app/src/main/java/com/ned/mycamera2/Camera2Fragment.java
+++ app/src/main/java/com/ned/mycamera2/Camera2Fragment.java
@@ -1,19 +1,47 @@
 package com.ned.mycamera2;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Size;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.Arrays;
 
 /**
  * Created by NedHuang on 2016/10/13.
  */
 
-public class Camera2Fragment extends Fragment {
+public class Camera2Fragment extends Fragment implements TextureView.SurfaceTextureListener {
 
     private static final String TAG = "Camera2Fragment";
+    private Camera2Fragment mInstance = this;
+    private Activity mAct = mInstance.getActivity();
+    private static final String sCameraId = "0";
+    private TextureView mTextureView = null;
+    private HandlerThread mHandlerThread = null;
+    private Handler mHandler = null;
+    private Size mPreviewSize = null;
+    private CaptureRequest.Builder mPreviewBuilder = null;
 
     public static Camera2Fragment newInstance() {
         return new Camera2Fragment();
@@ -27,6 +55,7 @@ public class Camera2Fragment extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
+        releaseLooper();
     }
 
     @Override
@@ -37,11 +66,125 @@ public class Camera2Fragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.camera_fragment_layout, container, false);
+        initLooper();
+        initUIAndListener(view);
+        return view;
+    }
+
+    private void initLooper() {
+        mHandlerThread = new HandlerThread("CAMERA2");
+        mHandlerThread.start();
+        mHandler = new Handler(mHandlerThread.getLooper());
+    }
+
+    private void releaseLooper() {
+        mHandlerThread.quitSafely();
+        try {
+            mHandlerThread.join();
+            mHandlerThread = null;
+            mHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initUIAndListener(View v) {
+        mTextureView = (TextureView) v.findViewById(R.id.texture_view);
+        mTextureView.setSurfaceTextureListener(mInstance);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+        try {
+            CameraManager cameraManager = (CameraManager) mAct.getSystemService(Context.CAMERA_SERVICE);
+            CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(sCameraId);
+            StreamConfigurationMap streamConfigurationMap = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            mPreviewSize = streamConfigurationMap.getOutputSizes(SurfaceTexture.class)[0];
+            if (ActivityCompat.checkSelfPermission(mAct, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            cameraManager.openCamera(sCameraId, mCameraDeviceStateCallback, mHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+    }
+
+    private CameraDevice.StateCallback mCameraDeviceStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice cameraDevice) {
+            try {
+                startPreview(cameraDevice);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice cameraDevice) {
+            cameraDevice.close();
+        }
+
+        @Override
+        public void onError(CameraDevice cameraDevice, int i) {
+            cameraDevice.close();
+        }
+
+        private void startPreview(CameraDevice cameraDevice) throws CameraAccessException {
+            SurfaceTexture surfaceTexture = mTextureView.getSurfaceTexture();
+            surfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            Surface surface = new Surface(surfaceTexture);
+            mPreviewBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mPreviewBuilder.addTarget(surface);
+            cameraDevice.createCaptureSession(Arrays.asList(surface), mSessionStateCallback, mHandler);
+        }
+    };
+
+    private CameraCaptureSession.StateCallback mSessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(CameraCaptureSession cameraCaptureSession) {
+            try {
+                updatePreview(cameraCaptureSession);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
+
+        }
+
+        private void updatePreview(CameraCaptureSession cameraCaptureSession) throws CameraAccessException {
+            cameraCaptureSession.setRepeatingRequest(mPreviewBuilder.build(), null, mHandler);
+        }
+    };
 }
